3b3a80eb91d98f634a389823787a7776720c140d
Ynda Jas <yndajas@gmail.com>
2020-07-05 19:24:07 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
+def my_collect(collection)
+  count = 0
+  new_collection = []
+  while count < collection.length do
+    new_collection << yield(collection[count])
+    count += 1
+  end
+  new_collection
+end
 
