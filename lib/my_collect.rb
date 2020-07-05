11e8e70fbe6815b77d1392db5680ab9e5e66c0c0
Ynda Jas <yndajas@gmail.com>
2020-07-05 19:21:27 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,10 @@
 def my_collect(collection)
   count = 0
+  new_collection = []
   while count < collection.length do
-    yield collection[count]
+    new_collection << yield collection[count]
     count += 1
   end
+  new_collection
 end
 
